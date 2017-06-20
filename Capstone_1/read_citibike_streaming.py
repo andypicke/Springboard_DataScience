485b261b76326c0ef3422177c96970f5deae02ac
andypicke <andypicke@gmail.com>
2017-06-20 15:04:37 -0600

    update code to read citibike feed
    time was in UTC; add NYC time and compute hour etc. from that
    Also save raw csv before doing anything to it

--- Capstone_1/read_citibike_streaming.py
+++ Capstone_1/read_citibike_streaming.py
@@ -21,18 +21,27 @@ dat = r.json() # returns dictionary
 # make dataframe of station data
 df=pd.DataFrame( dat['data']['stations'] )
 
-# add timestamp to rows
-df['timestamp']=pd.to_datetime(dat['last_updated'],unit='s')
+# write raw data to csv so we can always go back later if needed
+csv_name = '/Users/Andy/Springboard_DataScience/Capstone_1/data/feeds/' + str(pd.to_datetime(dat['last_updated'],unit='s')) + '.csv'
+df.to_csv(csv_name,index=False)
 
-df['year']=df.timestamp.dt.year
-df['hour']=df.timestamp.dt.hour
-df['yday']=df.timestamp.dt.dayofyear
+# add timestamp to rows (this is in UTC)
+#timestamp_utc = pd.to_datetime(dat['last_updated'],unit='s', utc=True )
+timestamp_utc = pd.to_datetime(dat['last_updated'],unit='s',utc=True)
+df['timestamp_utc'] = timestamp_utc.to_datetime64()
+#df['timestamp_utc'] = pd.to_datetime(df['timestamp_utc'] )
+
+# also add local (NYC) time
+timestamp_nyc = timestamp_utc.tz_convert(tz='America/New_York')
+df['timestamp_nyc'] = timestamp_nyc.to_datetime64()
+#df['timestamp_nyc'] = pd.to_datetime(df['timestamp_nyc'])
+
+# add yearh, hour, day of year (using NY time)
+df['year']=df.timestamp_nyc.dt.year
+df['hour']=df.timestamp_nyc.dt.hour
+df['yday']=df.timestamp_nyc.dt.dayofyear
 
 # write the data to sql database
 con = sqlite3.connect("/Users/Andy/Springboard_DataScience/Capstone_1/data/citibike_feeds.db3")
 df.to_sql("station_status",con,if_exists='append',index=False)
 con.close()
-
-# write to csv also
-csv_name = '/Users/Andy/Springboard_DataScience/Capstone_1/data/feeds/' + str(pd.to_datetime(dat['last_updated'],unit='s')) + '.csv'
-df.to_csv(csv_name,index=False)
