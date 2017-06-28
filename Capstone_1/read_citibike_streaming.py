be1c2bcd94d0d9d70a8072bfc445850db049e024
andypicke <andypicke@gmail.com>
2017-06-28 14:14:29 -0600

    write to S3 bucket also

--- Capstone_1/read_citibike_streaming.py
+++ Capstone_1/read_citibike_streaming.py
@@ -25,21 +25,34 @@ df=pd.DataFrame( dat['data']['stations'] )
 csv_name = '/Users/Andy/Springboard_DataScience/Capstone_1/data/feeds/' + str(pd.to_datetime(dat['last_updated'],unit='s')) + '.csv'
 df.to_csv(csv_name,index=False)
 
+# write the raw csv file to S3 also
+import boto3
+s3 = boto3.resource('s3')
+fname = csv_name
+key_name = 'station_status/' + str(pd.to_datetime(dat['last_updated'],unit='s')) + '.csv'
+data = open(fname, 'rb')
+s3.Bucket('citibikefeed').put_object(Key=key_name, Body=data)
+
 # add timestamp to rows (this is in UTC)
-#timestamp_utc = pd.to_datetime(dat['last_updated'],unit='s', utc=True )
+# note each station also has a 'last updated'?
 timestamp_utc = pd.to_datetime(dat['last_updated'],unit='s',utc=True)
+#timestamp_utc = pd.to_datetime(df['last_updated'],unit='s',utc=True)
+
 df['timestamp_utc'] = timestamp_utc.to_datetime64()
 #df['timestamp_utc'] = pd.to_datetime(df['timestamp_utc'] )
 
 # also add local (NYC) time
 timestamp_nyc = timestamp_utc.tz_convert(tz='America/New_York')
-df['timestamp_nyc'] = timestamp_nyc.to_datetime64()
+#df['timestamp_nyc'] = timestamp_nyc.to_datetime64()
 #df['timestamp_nyc'] = pd.to_datetime(df['timestamp_nyc'])
 
 # add yearh, hour, day of year (using NY time)
-df['year']=df.timestamp_nyc.dt.year
-df['hour']=df.timestamp_nyc.dt.hour
-df['yday']=df.timestamp_nyc.dt.dayofyear
+#df['year']=df.timestamp_nyc.dt.year
+#df['hour']=df.timestamp_nyc.dt.hour
+#df['yday']=df.timestamp_nyc.dt.dayofyear
+df['year']=timestamp_nyc.year
+df['hour']=timestamp_nyc.hour
+df['yday']=timestamp_nyc.dayofyear
 
 # write the data to sql database
 con = sqlite3.connect("/Users/Andy/Springboard_DataScience/Capstone_1/data/citibike_feeds.db3")
